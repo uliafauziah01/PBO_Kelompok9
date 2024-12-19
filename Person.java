b4e6a8acec8b87365ff961e5cb58fb723cd9cb0f
prafangasta <2300018378@webmail.uad.ac.id>
2024-12-19 19:36:55 +0700

    Person.java

--- Person.java
+++ Person.java
@@ -1,37 +1,14 @@
-public abstract class Person {
-    private String id;
-    private String nama;
-    private String alamat;
+public class Person {
+    protected String id;
+    protected String nama;
 
-    public Person(String id, String nama, String alamat) {
+    public Person(String id, String nama) {
         this.id = id;
         this.nama = nama;
-        this.alamat = alamat;
     }
 
-    public String getId() {
-        return id;
+    @Override
+    public String toString() {
+        return this.id + ", " + this.nama;
     }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-
-    public abstract void tampilkanInfo();
 }
