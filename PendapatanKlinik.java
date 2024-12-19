dbe1f9ade128ac77b5154c66309355ca736f8e63
prafangasta <2300018378@webmail.uad.ac.id>
2024-12-19 19:36:30 +0700

    PendapatanKlinik.java

--- PendapatanKlinik.java
+++ PendapatanKlinik.java
@@ -1,3 +1,20 @@
-public interface PendapatanKlinik {
-    void tampilkanPendapatan();
+public class PendapatanKlinik {
+    private double pendapatan;
+
+    public PendapatanKlinik() {
+        this.pendapatan = 0;
+    }
+
+    public void tambahPendapatan(double jumlah) {
+        this.pendapatan += jumlah;
+    }
+
+    public double getPendapatan() {
+        return pendapatan;
+    }
+
+    @Override
+    public String toString() {
+        return "Pendapatan Klinik: Rp " + pendapatan;
+    }
 }
