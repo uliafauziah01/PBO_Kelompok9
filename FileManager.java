0863a25989f9bda63b59b50ae733895aa4a60b79
Sekar22222 <2300018351@webmail.uad.ac.id>
2025-01-13 17:21:18 +0700

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -1,26 +1,5 @@
-import java.io.*;
 import java.util.List;
 
-public class FileManager {
-    public static void simpanData(String filename, List<String> data) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            for (String line : data) {
-                writer.write(line);
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error writing to file: " + e.getMessage());
-        }
-    }
-
-    public static void bacaData(String filename) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading file: " + e.getMessage());
-        }
-    }
+public interface FileManager {
+    void simpanData(String filename, List<String> data);
 }
