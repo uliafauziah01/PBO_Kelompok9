38b1867dfe0634f014c5e4869b61a490dd52ed2a
prafangasta <2300018378@webmail.uad.ac.id>
2025-01-13 17:30:51 +0700

    Update SistemManajemenKlinik.java

--- SistemManajemenKlinik.java
+++ SistemManajemenKlinik.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class SistemManajemenKlinik {
+public class SistemManajemenKlinik { 
     private List<Pasien> daftarPasien;
     private List<Dokter> daftarDokter;
     private List<Karyawan> daftarKaryawan;
