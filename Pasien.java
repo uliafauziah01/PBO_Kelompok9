f9ad37821fc82b45b91e72e52856de6c7be26eed
uliafauziah01 <sanstitan42@gmail.com>
2025-01-13 17:17:06 +0700

    Update Pasien.java

--- Pasien.java
+++ Pasien.java
@@ -1,18 +1,43 @@
 public class Pasien {
-    String id;
-    String nama;
-    String penyakit;
-    String alamat;
+    private static int counter = 1; // Counter untuk ID
+    private String id;
+    private String nama;
+    private String alamat;
+    private String penyakit;
+    private int usia;
 
-    public Pasien(String id, String nama, String penyakit, String alamat) {
-        this.id = id;
+    public Pasien(String nama, String alamat, String penyakit, int usia) {
+        this.id = generateId();
         this.nama = nama;
-        this.penyakit = penyakit;
         this.alamat = alamat;
+        this.penyakit = penyakit;
+        this.usia = usia;
+    }
+
+    private String generateId() {
+        return String.format("P%02d", counter++);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getPenyakit() {
+        return penyakit;
     }
 
     @Override
     public String toString() {
-        return "ID Pasien: " + id + ", Nama: " + nama + ", Penyakit: " + penyakit;
+        return "Pasien{" +
+                "id='" + id + '\'' +
+                ", nama='" + nama + '\'' +
+                ", alamat='" + alamat + '\'' +
+                ", penyakit='" + penyakit + '\'' +
+                ", usia=" + usia +
+                '}';
     }
 }
