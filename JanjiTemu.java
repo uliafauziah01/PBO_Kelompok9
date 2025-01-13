dde0b0a82ce0190802cb641bdaf1f4e5dc48ca26
Sekar22222 <2300018351@webmail.uad.ac.id>
2025-01-13 17:21:57 +0700

    Update JanjiTemu.java

--- JanjiTemu.java
+++ JanjiTemu.java
@@ -3,19 +3,32 @@ public class JanjiTemu {
     private Dokter dokter;
     private String penyakit;
     private Karyawan petugas;
+    private String hari;
 
-    public JanjiTemu(Pasien pasien, Dokter dokter, String penyakit, Karyawan petugas) {
+    public JanjiTemu(Pasien pasien, Dokter dokter, String penyakit, Karyawan petugas, String hari) {
         this.pasien = pasien;
         this.dokter = dokter;
         this.penyakit = penyakit;
         this.petugas = petugas;
+        this.hari = hari;
+    }
+
+    public Pasien getPasien() {
+        return pasien;
+    }
+
+    public Dokter getDokter() {
+        return dokter;
     }
 
     @Override
     public String toString() {
-        return "Rincian:\n" +
-               "ID Pasien: " + pasien.id + "\nNama: " + pasien.nama + "\nPenyakit: " + penyakit +
-               "\nKode Dokter: " + dokter.id + "\nNama Dokter: " + dokter.nama + "\nSpesialisasi: " + dokter.spesialisasi +
-               "\nJadwal Praktek: " + dokter.jadwalPraktek + "\nPetugas: ID " + petugas.id + ", " + petugas.nama + ", Jabatan: " + petugas.jabatan;
+        return "JanjiTemu{" +
+                "pasien=" + pasien.getNama() +
+                ", dokter=" + dokter.getNama() +
+                ", penyakit='" + penyakit + '\'' +
+                ", petugas=" + petugas.getNama() +
+                ", hari='" + hari + '\'' +
+                '}';
     }
 }
