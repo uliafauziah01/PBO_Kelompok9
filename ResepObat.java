4c1faed14d159b9a4d906bd7ec6c700dca14d2a2
sally15-cans <2300018346@webmail.uad.ac.id>
2025-01-13 17:24:56 +0700

    Update ResepObat.java

--- ResepObat.java
+++ ResepObat.java
@@ -1,18 +1,55 @@
 public class ResepObat {
     private Dokter dokter;
-    private String resep;
+    private String obat;
+    private String dosis;
+    private String instruksi;
 
-    public ResepObat(Dokter dokter, String resep) {
+    public ResepObat(Dokter dokter, String obat, String dosis, String instruksi) {
         this.dokter = dokter;
-        this.resep = resep;
+        this.obat = obat;
+        this.dosis = dosis;
+        this.instruksi = instruksi;
     }
 
-    public String getResep() {
-        return resep;
+    public Dokter getDokter() {
+        return dokter;
+    }
+
+    public void setDokter(Dokter dokter) {
+        this.dokter = dokter;
+    }
+
+    public String getObat() {
+        return obat;
+    }
+
+    public void setObat(String obat) {
+        this.obat = obat;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getInstruksi() {
+        return instruksi;
+    }
+
+    public void setInstruksi(String instruksi) {
+        this.instruksi = instruksi;
     }
 
     @Override
     public String toString() {
-        return "Resep Obat: " + resep;
+        return "ResepObat{" +
+                "dokter=" + dokter +
+                ", obat='" + obat + '\'' +
+                ", dosis='" + dosis + '\'' +
+                ", instruksi='" + instruksi + '\'' +
+                '}';
     }
 }
